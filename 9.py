eadea132bba6ef890b0d0b57d194a90bcbf86ba1
Lucute2001 <lch.ryu2001@gmail.com>
2023-04-26 22:21:06 +0700

    update 9.py

--- 9.py
+++ 9.py
@@ -1,16 +1,22 @@
 while True:
-    num = input("Nhập vào một số nguyên: ")
     try:
-        num = int(num)
-        if num < 0:
-            print("Số bạn nhập không phải là số âm. Vui lòng nhập lại.")
+        num = int(input("Nhập một số nguyên dương: "))
+        if num > 0:
+            print("Số bạn đã nhập là:", num)
+            break
         else:
-            print("Bạn vừa nhập số ", num)
-            break
+            print("Bạn cần nhập một số nguyên dương. Vui lòng nhập lại.")
     except ValueError:
-        try:
-            num = float(num)
-            print("Số bạn nhập là số thực.")
-            break
-        except ValueError:
-            print("Bạn đã không nhập một số hợp lệ. Vui lòng nhập lại.")
+        print("Bạn cần nhập một số nguyên dương. Vui lòng nhập lại.")
+
+'''
+Đoạn mã trên sử dụng vòng lặp while vô hạn
+để liên tục yêu cầu người dùng nhập một số.
+Nếu giá trị nhập vào là một số nguyên dương đúng định dạng, 
+thì giá trị đó sẽ được in ra màn hình và vòng lặp sẽ dừng lại bằng cách sử dụng lệnh break. 
+Nếu giá trị nhập vào không phải số nguyên dương, hoặc không phải một số hợp lệ, 
+thì thông báo lỗi sẽ được in ra và vòng lặp sẽ tiếp tục yêu cầu người dùng nhập lại.
+'''
+
+
+
